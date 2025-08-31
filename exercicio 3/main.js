3737897ad516c5e58ac11526993c5d320bc7ba39
LuskGPM <145815917+LuskGPM@users.noreply.github.com>
2025-08-31 14:26:35 -0300

    exe 3

---
+++ exercicio 3/main.js
@@ -0,0 +1,22 @@
+let inicio = document.getElementById('inicio')
+let fim = document.getElementById('fim')
+let passo = document.getElementById('passo')
+let btn = document.getElementById('button')
+let contando = document.getElementById('contando')
+
+
+btn.onclick = () => {
+    contando.innerHTML = 'Contando: <br>'
+    let i = Number(inicio.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+    if (i < f) {
+        for (let c = i; c <= f; c+=p) {
+            contando.innerHTML += ` \u{1F449} ${c}`
+        }
+    } else {
+        for (let c = i; c >= f; c-=p) {
+            contando.innerHTML += ` \u{1F449} ${c}`
+        }
+    }
+}
